51a0887f1fac1e7bdfd5216ddd9a1d75fa38789c
Hans Fangohr escape <hans.fangohr@xfel.eu>
2018-03-18 11:40:05 +0100

    test first

--- test_lib.py
+++ test_lib.py
@@ -42,3 +42,29 @@ def test_find_sections3():
 
 def test_find_sections_empty():
     assert l.sections_split("") == []
+
+
+def test_identify_clock_table():
+
+    # define test string
+    s = r"""
+#+BEGIN: clocktable :maxlevel 3 :scope subtree
+#+CAPTION: Clock summary at [2018-03-04 Sun 18:26]
+| Headline                    | Time   |      |      |
+|-----------------------------+--------+------+------|
+| *Total time*                | *9:05* |      |      |
+|-----------------------------+--------+------+------|
+| \emsp Week 1                |        | 9:05 |      |
+| \emsp\emsp <2018-03-01 Thu> |        |      | 4:35 |
+| \emsp\emsp <2018-03-02 Fri> |        |      | 4:30 |
+#+END:
+"""
+    meta, table = l.identify_clock_table(s)
+    assert meta['#+BEGIN:'] == "clocktable :maxlevel 3 :scope subtree"
+    assert meta['#+CAPTION:'] == "Clock summary at [2018-03-04 Sun 18:26]"
+    assert meta['#+END:'] == ""
+    assert table[0:10] == "| Headlin"
+    assert table[-10:] == "  | 4:30 |"
+
+def test_parse_clock_table():
+    raise NotImplementedError
